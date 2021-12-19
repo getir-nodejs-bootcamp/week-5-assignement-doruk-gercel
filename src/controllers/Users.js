15256ab5a531138fbca1df31c29482f7cf3872ae
DorukGercel <dorukgercel@hotmail.com>
2021-12-19 11:10:14 +0300

    Add reset password functionality

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,11 +1,13 @@
 const hs = require("http-status");
 const { list, insert, findOne } = require("../services/Users");
 const { passwordToHash, generateJWTAccessToken, generateJWTRefreshToken } = require("../scripts/utils/helper");
+const eventEmitter = require("../scripts/utils/events/EventEmitter");
+const error = require("../definitions/Error")
 
 const index = (req, res) => {
   list()
     .then((userList) => {
-      if (!userList) res.status(hs.INTERNAL_SERVER_ERROR).send({ error: "Sorun var.." });
+      if (!userList) res.status(hs.INTERNAL_SERVER_ERROR).send({ error: error.INTERNAL_ERROR});
       res.status(hs.OK).send(userList);
     })
     .catch((e) => res.status(hs.INTERNAL_SERVER_ERROR).send(e));
@@ -15,7 +17,7 @@ const create = (req, res) => {
   req.body.password = passwordToHash(req.body.password);
   insert(req.body)
     .then((createdUser) => {
-      if (!createdUser) res.status(hs.INTERNAL_SERVER_ERROR).send({ error: "Sorun var.." });
+      if (!createdUser) res.status(hs.INTERNAL_SERVER_ERROR).send({ error: error.INTERNAL_ERROR});
       res.status(hs.OK).send(createdUser);
     })
     .catch((e) => res.status(hs.INTERNAL_SERVER_ERROR).send(e));
@@ -25,7 +27,7 @@ const login = (req, res) => {
   req.body.password = passwordToHash(req.body.password);
   findOne(req.body)
     .then((user) => {
-      if (!user) return res.status(hs.NOT_FOUND).send({ message: "Böyle bir kullanıcı bulunmamaktadır." });
+      if (!user) return res.status(hs.NOT_FOUND).send({ message: error.USER_NOT_EXIST});
       user = {
         ...user.toObject(),
         tokens: {
@@ -39,12 +41,32 @@ const login = (req, res) => {
     .catch((e) => res.status(hs.INTERNAL_SERVER_ERROR).send(e));
 };
 
-//! ÖDEV Video Üzerinden izleyip implemente edilecek.
-// https://www.youtube.com/watch?v=pMi3PiITsMc
-const resetPassword = () => {};
+// HOMEWORK!!!
+const resetPassword = (req, res) => {
+  // Generate password
+  const newPassword = uuid.v4()?.split("-")[0] || `usr-${new Date().getTime()}`;
+  modify({ email: req.body.email }, { password: passwordToHash(newPassword) })
+    .then((updatedUser) => {
+      if (!updatedUser)
+        return res.status(hs.NOT_FOUND).send({
+          error: error.USER_NOT_EXIST
+        });
+      // Send mail
+      eventEmitter.emit("send_email", {
+        to: updatedUser.email,
+        subject: "Password Reset Request",
+        html: `Your one time password is ${newPassword}<br/> Please change this password after login.`
+      });
+      res.status(hs.OK).send({ message: "Generated password sent to email." });
+    })
+    .catch(() => {
+      res.status(hs.INTERNAL_SERVER_ERROR).send({ error: error.INTERNAL_ERROR });
+    });
+};
 
 module.exports = {
   index,
   create,
   login,
+  resetPassword
 };
